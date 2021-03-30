23f4ddef986c1a2dea0d81e5103a51f201d44c12
Danu Mahendra <80309128+danudevphoton@users.noreply.github.com>
2021-03-30 17:33:10 +0700

    update

--- test/HelloWorld.Tests/BookTest.cs
+++ test/HelloWorld.Tests/BookTest.cs
@@ -8,14 +8,14 @@ namespace HelloWorld.Tests
         [Fact]
         public void BookCalculateAvg()
         {
-            var book = new Book("");
+            var book = new InMemoryBook("");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.3);
             var result = book.GetStatistics();
-            Assert.Equal(85.6,result.average);
-            Assert.Equal(90.5,result.high);
-            Assert.Equal(77.3,result.low); 
+            Assert.Equal(85.6,result.average,1);
+            Assert.Equal(90.5,result.high,1);
+            Assert.Equal(77.3,result.low,1); 
         }
     }
 }
--- test/HelloWorld.Tests/TypeTest.cs
+++ test/HelloWorld.Tests/TypeTest.cs
@@ -3,99 +3,160 @@ using Xunit;
 
 namespace HelloWorld.Tests
 {
-    public class Person {
-
-    }
-
-    public struct Point {
-
-    }
-
     public delegate string WriteLogDelegate(string logMessage);
 
     public class TypeTests
     {
-        int count = 0;
         [Fact]
-        public void WriteLogCanMethod(){
-            WriteLogDelegate log = ReturnMessage;
-            log += ReturnMessage;
-            log += IncrementCount;
-            var result = log("Hello");
-            Assert.Equal("Hello", result);
+        public void WriteLogDelegateCanPointToMethod()
+        {
+            WriteLogDelegate log;
+
+            // log = new WriteLogDelegate(ReturnMessage);
+            log = ReturnMessage;
+
+            var result = log("Hello!");
+            Assert.Equal("Hello!", result);
         }
 
-        string IncrementCount(string message){   
-            count++;
-            return message.ToLower();
-        }
-
-        string ReturnMessage(string message){   
-            count++;
+        string ReturnMessage(string message)
+        {
             return message;
         }
 
         [Fact]
-        public void Test1(){
+        public void ValueTypeAlsoPassByRef()
+        {
             var x = GetInt();
             SetInt(ref x);
+
             Assert.Equal(42, x);
         }
 
-        private void SetInt(ref Int32 x){
+        private void SetInt(ref int x)
+        {
             x = 42;
         }
 
-        private int GetInt(){
+        [Fact]
+        public void ValueTypeAlsoPassByValue()
+        {
+            var x = GetInt();
+            SetInt(x);
+
+            Assert.Equal(3, x);
+        }
+
+        private void SetInt(int x)
+        {
+            x = 42;
+        }
+
+        private int GetInt()
+        {
             return 3;
         }
-
+        
         [Fact]
-        public void StringsBehaveLike(){
-            string name = "Scott";
-            var getname = MakeUpperCase(name);
-
-            Assert.Equal("SCOTT",getname);
-        }
-
-        private string MakeUpperCase(string param){
-            return param.ToUpper();
-        }
-
-        [Fact]
-        public void CanSetNameForRefer()
+        public void CSharpIsPassByRef()
         {
+            // Arrange
             var book1 = GetBook("Book 1");
-            SetName(out book1, "New Name");
-            Assert.Equal("New Name",book1.Name);
+            GetBookSetName(ref book1, "New Name");
+
+            // Act
+            
+            // Assert
+            Assert.Equal("New Name", book1.Name);
         }
 
-        private void SetName(out Book book, string name){
-            //book.Name = name;
-            book = new Book(name);
-        }
-
-        [Fact]
-        public void GetBookReturnDiffObj() {
-            var book1 = GetBook("1");
-            var book2 = GetBook("2");
-
-            Assert.Equal("1",book1.Name);
-            Assert.Equal("2",book2.Name);
-            Assert.NotSame(book1,book2);
+        private void GetBookSetName(ref InMemoryBook book, string name)
+        {
+            book = new InMemoryBook(name);
         }
 
         [Fact]
-        public void TwoVarRefer() {
-            var book1 = GetBook("1");
+        public void CSharpIsPassByValue()
+        {
+            // Arrange
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            // Act
+            
+            // Assert
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        private void GetBookSetName(InMemoryBook book, string name)
+        {
+            book = new InMemoryBook(name);
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            // Arrange
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            // Act
+            
+            // Assert
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void SetName(InMemoryBook book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Scott";
+            MakeUpperCase(name);
+
+            Assert.Equal("Scott", name);
+        }
+
+        private void MakeUpperCase(string name)
+        {
+            name.ToUpper();
+        }
+
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            // Arrange
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
+
+            // Act
+            
+            // Assert
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
+        }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            // Arrange
+            var book1 = GetBook("Book 1");
             var book2 = book1;
 
-            Assert.Same(book1,book2);
-            Assert.True(Object.ReferenceEquals(book1,book2));
+            // Act
+            
+            // Assert
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
         }
 
-        Book GetBook(string name) {
-            return new Book(name);
-        }   
+        private InMemoryBook GetBook(string name)
+        {
+            return new InMemoryBook(name);
+        }
     }
 }
